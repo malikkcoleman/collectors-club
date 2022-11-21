d53c41aacdb2e1971277afaa54d31f62370deac5
malikkcoleman <malikcoleman@live.ca>
2022-11-21 12:02:14 -0600

    Adding validations and foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_21_171617) do
+ActiveRecord::Schema.define(version: 2022_11_21_175805) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2022_11_21_171617) do
     t.integer "qty"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "products", "categories"
 end
